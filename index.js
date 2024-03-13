5f0dd98c619a7b6c3cd13b23bc01bec6f2969bdc
colecody27 <colecody27@gmail.com>
2024-03-12 20:33:30 -0700

    Update file structure and add EJS library

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 // const mongoose = require('mongoose');
 const express = require('express');
-
 const app = express(); 
 const port = 8080;
 
+app.set('view engine', 'ejs')
+app.use(express.json())
 app.use(express.static('public'))
+
+const loginRouter = require('./src/routes/login')
+app.use('/login', loginRouter)
+
 app.listen(port, () => {
     console.log(`Server live on: http://localhost:${port}`)
 }) 
+
+
 
